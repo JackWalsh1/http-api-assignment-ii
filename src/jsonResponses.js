47c2719da0a9ce49ff71db931169d34e70859d42
Jack Walsh <jacksonwalsh931@gmail.com>
2023-02-19 20:52:42 -0500

    get / head are all working with no users

---
+++ src/jsonResponses.js
@@ -0,0 +1,74 @@
+// 'database' of all users
+const users = {};
+
+// response objects
+const respond = (request, response, status, content) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(content));
+  response.end();
+};
+
+const respondMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+// GET METHODS
+const getUsers = (request, response) => {
+
+  console.log("in getUsers");
+
+  const responseJSON = {
+    users,
+  }
+
+  return respond(request, response, 200, responseJSON);
+};
+
+const notReal = () => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return obj with
+  return respond(request, response, 404, responseJSON);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return obj with
+  return respond(request, response, 404, responseJSON);
+};
+
+// HEAD METHODS
+const getUsersMeta = (request, response) => {
+  respondMeta(request, response, 200);
+};
+
+const notRealMeta = (request, response) => {
+  respondMeta(request, response, 404);
+};
+
+const notFoundMeta = (request, response) => {
+  respondMeta(request, response, 404);
+};
+
+// POST METHODS
+const addUsers = () => {
+
+};
+
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  notReal,
+  notRealMeta,
+  notFound,
+  notFoundMeta,
+  addUsers,
+};
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
-const statusHandler = require('./statusResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -10,32 +10,39 @@ const routes = {
   GET: {
     '/': htmlHandler.getIndex,
     '/style.css': htmlHandler.getIndexCSS,
-    '/getUsers': statusHandler.getUsers,
-    '/notReal': statusHandler.notReal,
-    notFound: statusHandler.notFound,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notReal,
+    notFound: jsonHandler.notFound,
   },
   HEAD: {
-    '/getUsers': statusHandler.getUsersMeta,
-    '/notReal': statusHandler.notRealMeta,
-    notFound: statusHandler.notFoundMeta,
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notRealMeta,
+    notFound: jsonHandler.notFoundMeta,
   },
   POST: {
-    '/addUser': statusHandler.addUser,
-  }
+    '/addUser': jsonHandler.addUsers,
+  },
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
-  const params = query.parse(parsedUrl.query);
+  console.log(routes[request.method][parsedUrl.pathname]);
 
+  // if request method does not exist
   if (!routes[request.method]) {
+    // notFound head
     return routes.HEAD.notFound(request, response);
   }
 
+    // if path exists
   if (routes[request.method][parsedUrl.pathname]) {
-    return routes[request.method][parsedUrl.pathname](request, response, params);
+    console.log("found path uwu");
+    // go to there
+    return routes[request.method][parsedUrl.pathname](request, response);
   }
+
+  // otherwise return notFound for that
   return routes[request.method].notFound(request, response);
 };
 
